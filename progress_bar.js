93c953c192be42f2655642ecb9d9d7029ed3c383
annie <princessannierojas@thelewiscollege.edu.ph>
2024-04-20 00:06:13 +0800

    first commit

---
+++ progress_bar.js
@@ -0,0 +1,239 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#javascript', {
+        strokeWidth: 4, 
+        easing: 'easeInOut', 
+        duration: 1000,  
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold',
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.9);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#html-css', {
+        strokeWidth: 4, 
+        easing: 'easeInOut', 
+        duration: 1000, 
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.9);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#python', {
+        strokeWidth: 4, 
+        easing: 'easeInOut',
+        duration: 1000,
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.9);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#java', {
+        strokeWidth: 4, 
+        easing: 'easeInOut',
+        duration: 1000,
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.7);
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#react', {
+        strokeWidth: 4, 
+        easing: 'easeInOut', 
+        duration: 1000, 
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.9);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#node', {
+        strokeWidth: 4, 
+        easing: 'easeInOut',
+        duration: 1000,
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.8);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#express', {
+        strokeWidth: 4, 
+        easing: 'easeInOut',
+        duration: 1000,
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.7);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var jsProgressBar = new ProgressBar.Line('#apis', {
+        strokeWidth: 4, 
+        easing: 'easeInOut',
+        duration: 1000,
+        color: 'black', 
+        trailColor: 'gray', 
+        trailWidth: 4, 
+        svgStyle: {width: '100%', height: '100%'}, 
+        text: {
+            style: {
+                position: 'absolute',
+                right: '0',
+                top: '3',
+                padding: '5px 10px',
+                fontSize: '12px',
+                color: 'white',
+                fontWeight: 'bold'
+            }
+        },
+        step: function(state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); 
+        }
+    });
+
+    jsProgressBar.animate(0.7);
+});
+
+
+
+
+
+
+
+
+
+
+
+
